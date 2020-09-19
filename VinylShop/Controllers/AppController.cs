a52431406bc69347d5fb0bac2d25c5c8fd5bba3a
Kamil <kamilsalega@gmail.com>
2020-09-19 06:05:26 +0200

    KER-010 - Build the Header and Footer

---
+++ VinylShop/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VinylShop.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Skontaktuj się z nami";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "O nas";
+            return View();
+        }
+    }
+}
--- VinylShop/Startup.cs
+++ VinylShop/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,13 +17,50 @@ namespace VinylShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddDbContext<AppDbContext>(options =>
+            //     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //
+            // services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+            //
+            // services.AddScoped<IPieRepository, PieRepository>();
+            // services.AddScoped<ICategoryRepository, CategoryRepository>();
+            // services.AddScoped<IOrderRepository, OrderRepository>();
+            // services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+            //app.UseNodeModules();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //Add Error Page
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSession();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute(
+                    name: "Default",
+                    pattern: "{controller}/{action}/{id?}",
+                    new {controller = "App", action = "Index"}
+                );
+                cfg.MapRazorPages();
+            });
         }
     }
 }
